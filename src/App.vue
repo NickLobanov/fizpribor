1fda20c0466b5817aaa4a1681cf153b5d626ed64
Nick Lobanov <nk.nicolas142@gmail.com>
2021-01-19 17:53:20 +0300

    create NavPanel component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <main>
+    <main class="main">
       <ModulePanel />
+      <NavPanel />
     </main>
   </div>
 </template>
@@ -10,22 +11,28 @@
 <script>
 import Header from '@/components/Header.vue'
 import ModulePanel from '@/components/ModulePanel.vue'
+import NavPanel from '@/components/NavPanel.vue'
 
 export default {
   name: 'App',
   components: {
    Header,
    ModulePanel,
+   NavPanel
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .main {
+    display: flex;
+  }
 </style>
---
+++ src/components/NavPanel.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav class="navbar">
+        <h1 class="navbar__title">Администрирование</h1>
+        <ul class="navbar__list">
+            <li>Общий вид</li>
+            <ul>
+                <li>Дэшборд</li>
+                <li>Аналитика</li>
+                <li>Системная информация</li>
+                <li>Проекты</li>
+            </ul>
+            <li>Управление доступом</li>
+            <ul>
+                <li>Пользователи</li>
+                <li>Организации</li>
+                <li>Группы</li>
+                <li>Роли</li>
+            </ul>
+            <li>Интеграция</li>
+            <ul>
+                <li>Импорты</li>
+                <li>Интеграция с электронной почтой</li>
+            </ul>
+            <li>Параметры сервера</li>
+            <ul>
+                <li>Глобальные параметры</li>
+                <li>Резервное копирование БД</li>
+                <li>Статистика</li>
+            </ul>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+    
+    }
+</script>
+
+<style scoped>
+    .navbar__list {
+        list-style: none;
+        border-left: 1px solid black;
+    }
+
+</style>
