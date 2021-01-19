736af53eaac21c0e1ca3a34d142db6180ec49e5a
Nick Lobanov <nk.nicolas142@gmail.com>
2021-01-19 17:57:16 +0300

    create NavList component

--- src/components/NavPanel.vue
+++ src/components/NavPanel.vue
@@ -1,39 +1,16 @@
 <template>
     <nav class="navbar">
         <h1 class="navbar__title">Администрирование</h1>
-        <ul class="navbar__list">
-            <li>Общий вид</li>
-            <ul>
-                <li>Дэшборд</li>
-                <li>Аналитика</li>
-                <li>Системная информация</li>
-                <li>Проекты</li>
-            </ul>
-            <li>Управление доступом</li>
-            <ul>
-                <li>Пользователи</li>
-                <li>Организации</li>
-                <li>Группы</li>
-                <li>Роли</li>
-            </ul>
-            <li>Интеграция</li>
-            <ul>
-                <li>Импорты</li>
-                <li>Интеграция с электронной почтой</li>
-            </ul>
-            <li>Параметры сервера</li>
-            <ul>
-                <li>Глобальные параметры</li>
-                <li>Резервное копирование БД</li>
-                <li>Статистика</li>
-            </ul>
-        </ul>
+        <NavList />
     </nav>
 </template>
 
 <script>
+    import NavList from '@/components/NavList.vue'
     export default {
-    
+        components: {
+           NavList, 
+        }
     }
 </script>
 
