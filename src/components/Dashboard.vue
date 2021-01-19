b97cd0150de84861c7065e03d0e7c6e9f094b5d9
Nick Lobanov <nk.nicolas142@gmail.com>
2021-01-19 21:11:11 +0300

    add dashboardItemWithoutProject

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,19 @@
 <template>
     <section class="dashboard">
         <DashboardItemWithProject />
+        <DashboardItemWithoutProject />
+        <DashboardItemWithProject />
+        <DashboardItemWithoutProject />
     </section>
 </template>
 
 <script>
     import DashboardItemWithProject from '@/components/DashboardItemWithProject.vue'
+    import DashboardItemWithoutProject from '@/components/DashboardItemWithoutProject.vue'
     export default {
         components: {
             DashboardItemWithProject,
+            DashboardItemWithoutProject
         }
     }
 </script>
---
+++ src/components/DashboardItemWithoutProject.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="dashboard-item">
+        <DashboardHeader />
+        <div class="dashboard-item__statistics">
+            <h2 class="dashboard-item__title">Статистика</h2>
+        </div>
+        <div class="dashboard-item__empty-first"></div>
+        <div class="dashboard-item__empty-second"></div>
+    </div>
+</template>
+
+<script>
+    import DashboardHeader from '@/components/DashboardHeader.vue'
+    export default {
+        components: {
+            DashboardHeader
+        }
+    }
+</script>
+
+<style scoped>
+    .dashboard-item {
+        width: 342px;
+    }
+
+    .dashboard-item__statistics {
+        width: 342px;
+        min-height: 226px;
+        margin-bottom: 12px;
+        background-color: #ffffff;
+    }
+
+    .dashboard-item__title {
+        font-size: 16px;
+        line-height: 22px;
+        color: #474747;
+        text-align: left;
+        padding-top: 6px;
+        padding-left: 25px;
+        margin-bottom: 12px;
+    }
+
+    .dashboard-item__empty-first {
+        width: 342px;
+        min-height: 168px;
+        background-color: #ffffff;
+    }
+
+    .dashboard-item__empty-first {
+        width: 342px;
+        min-height: 248px;
+        background-color: #ffffff;
+    }
+</style>
