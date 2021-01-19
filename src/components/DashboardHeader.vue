6d6343c0e3498161d5e9de2a9f59e18aa1ea9e9b
Nick Lobanov <nk.nicolas142@gmail.com>
2021-01-19 20:26:01 +0300

    create dashboardHeader

---
+++ src/components/DashboardHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="dashboard-header">
+        <h2 class="dashboard-header__title"></h2>
+        <button class="dashboard-header__button"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .dashboard-header {
+        width: 342px;
+    }
+
+    .dashboard-header__title {
+        font-size: 16px;
+        line-height: 22px;
+        color: #474747;        
+    }
+
+    .dashboard-header__button {
+        width: 299px;
+        margin: 0 auto;
+        padding-top: 5px;
+        padding-bottom: 9px;
+        background: #8292A5;
+        border: none;
+        border-radius: 3px;
+    }
+</style>
