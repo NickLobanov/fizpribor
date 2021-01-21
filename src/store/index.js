f9ebe7e03589a9a40028fbce06a20d65824b8237
Nick Lobanov <nk.nicolas142@gmail.com>
2021-01-21 15:55:06 +0300

    create store

--- src/store/index.js
+++ src/store/index.js
@@ -3,4 +3,41 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({})
+export default new Vuex.Store({
+    actions: {},
+    mutations: {},
+    state: {
+        users: [
+            {
+                name: 'Алексей',
+                secondName: 'Белянин',
+                patronymic: 'Васильевич',
+                login: 'BelianinA',
+                email: 'BelianinA@fizpribor.ru',
+                role: 'Разработчик'
+            }
+        ],
+        organizations: [
+            {
+                name: 'ООО «Физприбор»',
+                project: 'АЭС Аккую, АЭС Нововоронежская',
+                responible: 'Руководитель'
+            }
+        ],
+        groups: [
+            {
+                name: 'ООО «Физприбор»',
+                project: 'АЭС Аккую, АЭС Нововоронежская',
+                competitors: 5
+            }
+        ],
+        role: [
+            {
+                name: 'Разработчик',
+                keyName: 'developer',
+                functions: ['Конфигуратор БД (редактирование)', 'Редактор блоков (редкатирование)']  
+            }
+        ]
+    },
+    getters: {}
+})
